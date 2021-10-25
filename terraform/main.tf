12c61bf7a037cd365ee3ef86ee6b8c997e8b8e18
nizar <nizar.dev01@gmail.com>
2021-10-25 11:55:45 +0530

    Updated user_data to configure and install packages on EC2 instances.

--- terraform/main.tf
+++ terraform/main.tf
@@ -183,10 +183,23 @@ resource "aws_instance" "application-server-1" {
 
   user_data = <<-EOF
                 #!/bin/bash
-                sudo apt update -y
-                sudo apt-get update && sudo apt-get install build-essential python3 python3-pip nginx -y && pip3 install uwsgi
-                sudo systemctl start nginx
-                echo "<h1>One</h1>" > /var/www/html/index.nginx-debian.html
+                sudo apt-get update &&  sudo apt-get install build-essential git python3 python3-pip python3-venv nginx -y && pip3 install uwsgi
+                python3 -m venv ~/app && source ~/app/bin/activate
+                pip3 install django
+                git clone https://github.com/nizar-dev01/baby-django.git
+                sudo cp ~/baby-django/server.conf /etc/nginx/sites-available
+                sudo ln -s /etc/nginx/sites-available/server.conf /etc/nginx/sites-enabled
+
+                sudo sed -i 's/# server_names_hash_bucket_size 64;/server_names_hash_bucket_size 128;/g' /etc/nginx/nginx.conf
+                sudo service nginx restart
+                uwsgi --ini ~/baby-django/app_uwsgi.ini
+
+                mkdir ~/app/vassals
+                sudo cp ~/baby-django/emperor.uwsgi.service /etc/systemd/system
+
+                sudo systemctl enable emperor.uwsgi.service
+                sudo systemctl start emperor.uwsgi.service
+
                 EOF
   tags = {
     Name = "web-server-1"
@@ -204,10 +217,24 @@ resource "aws_instance" "application-server-2" {
 
   user_data = <<-EOF
                 #!/bin/bash
-                sudo apt update -y
-                sudo apt-get update && sudo apt-get install build-essential python3 python3-pip nginx -y && pip3 install uwsgi
-                sudo systemctl start nginx
-                echo "<h1>Two</h1>" > /var/www/html/index.nginx-debian.html
+                sudo apt-get update &&  sudo apt-get install build-essential git python3 python3-pip python3-venv nginx -y && pip3 install uwsgi
+                python3 -m venv ~/app && source ~/app/bin/activate
+                pip3 install django
+                git clone https://github.com/nizar-dev01/baby-django.git
+                sudo cp ~/baby-django/server.conf /etc/nginx/sites-available
+                sudo ln -s /etc/nginx/sites-available/server.conf /etc/nginx/sites-enabled
+
+                sudo sed -i 's/# server_names_hash_bucket_size 64;/server_names_hash_bucket_size 128;/g' /etc/nginx/nginx.conf
+                sudo service nginx restart
+                uwsgi --ini ~/baby-django/app_uwsgi.ini
+
+                mkdir ~/app/vassals
+                
+                sudo cp ~/baby-django/emperor.uwsgi.service /etc/systemd/system
+
+                sudo systemctl enable emperor.uwsgi.service
+                sudo systemctl start emperor.uwsgi.service
+                
                 EOF
   tags = {
     Name = "web-server-2"
